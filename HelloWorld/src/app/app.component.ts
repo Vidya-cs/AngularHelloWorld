6e26e5f6f251481a012420b69ffbfa2630d5cbbf
Vidya Sagar Singh <vidyasagarsingh6171@gmail.com>
2025-03-21 23:54:28 +0530

    [Vidya Sagar] Add: Completed UC4

---
+++ HelloWorld/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Helloworld';
+  imgUrl = "assets/BL_logo_square_png.png"; 
+  url = "https://www.bridgelabz.com";
+  userName: string = ""; // User input binding
+
+  onClick(event: Event): void {
+    console.log("BridgeLabz logo clicked!", event);
+    window.open(this.url, "_blank");
+  }
+}
